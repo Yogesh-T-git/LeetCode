7d0651c99ff50b4c90d206727e792e5c8e38e7f0
Yogesh T <yogesht.cs19@bmsce.ac.in>
2022-05-11 06:26:49 +0530

    Time: 1 ms (99.63%), Space: 42.4 MB (88.78%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,39 @@
+/*class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int[] arr = new int[2];
+        
+        for(int i= 0; i<nums.length; i++)
+            for(int j=i+1; j<nums.length; j++)
+                if(target == nums[i]+nums[j]){
+                    
+                    arr[0] = i;
+                    arr[1] = j;
+                    
+                }
+                   
+        return arr;
+    }
+}*/
+
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        
+        Map<Integer, Integer> m = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int compli = target - nums[i];
+            if(m.containsKey(compli)){
+                return new int[]{m.get(compli),i};
+            }
+            
+        
+            m.put(nums[i],i);
+        }
+       
+        throw new IllegalArgumentException("no match found");
+    }
+    
+}
