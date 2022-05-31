3429529d84a544a298e5b5c0ca91f76e989e6e54
Yogesh T <yogesht.cs19@bmsce.ac.in>
2022-05-31 06:41:24 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (94.22%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -14,7 +14,6 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         
         if (head == null || head.next == null) return head;
-        
         ListNode lastNode = reverseList(head.next);
         head.next.next = head;
         head.next = null;
